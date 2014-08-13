e94f48e575f8fbff201339afb08b4fc816ae787c
Javier Cervantes <javier@hybridgroup.com>
2014-08-13 17:23:50 -0500

    step 4

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,14 +1,21 @@
 'use strict';
 
 describe('PhoneListCtrl', function(){
+  var scope, ctrl;
 
   beforeEach(module('phonecatApp'));
 
-  it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope = {},
+  beforeEach(inject(function($controller) {
+    scope = {},
     ctrl = $controller('PhoneListCtrl', {$scope:scope});
-
-    expect(scope.phones.length).toBe(3);
   }));
+
+  it('should create "phones" model with 3 phones', function($controller) {
+    expect(scope.phones.length).toBe(3);
+  });
+
+  it('should set the default value of orderProp model', function() {
+    expect(scope.orderProp).toBe('age');
+  });
 
 });
