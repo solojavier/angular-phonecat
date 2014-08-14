9233e86ca7b236f86483efe6e1a5998cdd93f03e
Javier Cervantes <javier@hybridgroup.com>
2014-08-14 11:44:39 -0500

    step 9

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,7 @@
 'use strict';
 
-/* Filters */
+angular.module('phonecatFilters', []).filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+});
